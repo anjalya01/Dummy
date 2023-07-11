805cdec0c059a0d14907c5bf46ae90c03521dafb
Anjaly <anjalya01@gmail.com>
2023-07-11 21:24:03 +0530

    excelfile

---
+++ DummyExcelRead/src/main/java/TestExcel.java
@@ -0,0 +1,30 @@
+/*import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TestExcel {
+	
+	static FileInputStream f;
+	static XSSFWorkbook w;
+	static XSSFSheet s;
+	
+	public static String getstring(String a,String b) {
+		f=new FileInputStream("");
+		w=new XSSFWorkbook(f);
+		s=w.getSheet("");
+		row r=s.getRow(0);
+		Cell c=r.getCell
+				return c.getStringCellValue()
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+String d=ExcelCode.getStringData(0, 0)
+	}
+
+}
+*/
